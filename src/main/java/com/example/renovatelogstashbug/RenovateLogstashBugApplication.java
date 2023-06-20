bc7102b6da6df9a80bcf05807df12bc53111ad64
Stefan Ruf <stefan.ruf@inxmail.de>
2023-06-20 12:48:04 +0200

    Initial commit

---
+++ src/main/java/com/example/renovatelogstashbug/RenovateLogstashBugApplication.java
@@ -0,0 +1,14 @@
+package com.example.renovatelogstashbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RenovateLogstashBugApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( RenovateLogstashBugApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/example/renovatelogstashbug/RenovateLogstashBugApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.renovatelogstashbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class RenovateLogstashBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
